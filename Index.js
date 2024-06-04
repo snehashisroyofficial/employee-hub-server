faeb4cea5ac40b62fec43dde164f5720d9dc7625
Snehashis Roy <116051236+snehashisroyofficial@users.noreply.github.com>
2024-06-04 09:51:06 +0530

    dashboard role api created

--- Index.js
+++ Index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -8,8 +10,53 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.8kmx02i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const userCollection = client.db("EmployeeHub").collection("user");
+
+    app.post("/users", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await userCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const data = req.params.email;
+      console.log(data);
+      const email = { email: data };
+      const result = await userCollection.findOne(email);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
 app.get("/", (req, res) => {
-  res.send("Exployee Hub Server is running");
+  res.send("Employee Hub Server is running");
 });
 
 app.listen(port, () => {
